06bd06d8560e1ded1b34414e2d8ee9b8491bda79
John Levandowski <john@johnlevandowski.com>
2021-10-10 10:17:18 -0600

    update git repository move

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ reflector --country US --age 6 --sort rate --save /etc/pacman.d/mirrorlist
 pacstrap /mnt base linux linux-lts linux-firmware nano git
 genfstab -U /mnt >> /mnt/etc/fstab
 sed -i 's/relatime/noatime/' /mnt/etc/fstab
-mv /arch.johnlevandowski.com /mnt/arch.johnlevandowski.com
+mv /root/arch.johnlevandowski.com /mnt/arch.johnlevandowski.com
